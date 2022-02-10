7561cf78f3471f0ef61625926a4646676b491cea
rohit389negi <rohit389negi@gmail.com>
2022-02-10 21:28:32 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const urlController = require('../Controllers/urlController')
+const urlController = require('../controllers/urlController')
 
 
 router.post('/url/shorten', urlController.urlShortener)
